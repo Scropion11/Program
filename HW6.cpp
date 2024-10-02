5c6636ada1b774f0a83b8266d3dde67ba6c3cce1
tom86531 <66286697+tom86531@users.noreply.github.com>
2024-10-02 11:04:49 +0800

    Add files via upload

---
+++ HW6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a=9,b=2;
+	int c=a+b;
+	cout << "c = "<< c << endl;
+	int d=a-b;
+	cout << "d = "<< d << endl;
+	int e=a*b;
+	cout << "e = "<< e << endl;
+	int f=a/b;
+	cout << "f = "<< f << endl;
+	int g=a%5;
+	cout << "g = "<< g << endl;
+	system("pause");
+	return 0;
+} 
