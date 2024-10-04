17bf4d27a27c519287670476fe6a8b73b810818c
Scropion11 <66286697+Scropion11@users.noreply.github.com>
2024-10-04 17:26:43 +0800

    Add files via upload

---
+++ HW10.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a = 65,b = 66, c = 67,d = 68;
+	cout << char(a) << endl;
+	cout << char(b) << endl;
+	cout << char(c) << endl;
+	cout << char(d) << endl;
+	system("pause");
+}
