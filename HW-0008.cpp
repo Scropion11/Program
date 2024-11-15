1c9ccbb8cdb9357335ba424e079c6dc42174290a
Scropion11 <66286697+Scropion11@users.noreply.github.com>
2024-11-15 23:31:02 +0800

    Update and rename HW8.cpp to HW-0008.cpp

---
+++ HW-0008.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a=9,b=2;
+	cout << "ÅÜ´««e a = "<< a << endl;
+	cout << "ÅÜ´««e b = "<< b << endl;
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+	cout << "ÅÜ´««á a = "<< a << endl;
+	cout << "ÅÜ´««á b = "<< b << endl;
+	system("pause");
+}
